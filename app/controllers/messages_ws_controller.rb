a7bc652e30aa5944260c57b45c2103e3541b0770
osxi <osxi@users.noreply.github.com>
2015-08-29 21:45:34 -0500

    use subscription for messages

--- app/controllers/messages_ws_controller.rb
+++ app/controllers/messages_ws_controller.rb
@@ -3,6 +3,7 @@ class MessagesWsController < WebsocketRails::BaseController
     new_message = Message.new message
 
     if new_message.save!
+      WebsocketRails[:messages].trigger 'new', new_message
       trigger_success new_message
     else
       trigger_failure new_message
